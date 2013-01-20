e191b6cd269c9b5d1b2ae03c778dd97f5fa6da62
Toni Petrina <toni.petrina@gmail.com>
2013-01-20 20:57:52 +0100

    Added HashSet for Windows Phone 7
    + a few unit tests

---
+++ wp7-helpers/Common.UnitTests/HashSetTests/HashSetTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Common.UnitTests.HashSetTests
+{
+    [TestClass]
+    public class HashSetTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            var hs = new HashSet<string>();
+            Assert.IsTrue(hs.Add("hello"));
+        }
+
+        [TestMethod]
+        public void TestAddTwice()
+        {
+            var hs = new HashSet<string>();
+            hs.Add("hello");
+            Assert.IsFalse(hs.Add("hello"));
+        }
+    }
+}
